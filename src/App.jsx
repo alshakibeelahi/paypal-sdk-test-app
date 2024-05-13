b4d9d8506f8d9733a3703eb30f55b0d3e145401a
alshakibeelahi <alshakibeelahi@gmail.com>
2024-05-13 23:20:35 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PayPalButton from './PayPal';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test PayPal Integration</h1>
+      <PayPalButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PayPal.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+
+const PayPalButton = () => {
+  const paypalOptions = {
+    clientId: 'AVmEAzGogaeT0pVHi-N0OviOUVE4N1-7cwVcZBI_QA9SLNBTlXg--o2gf9a1cCc8nly90Q4n4VNa4N3D',
+    currency: 'USD',
+  };
+
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [{
+        amount: {
+          value: '10.00', // Enter the amount to be paid
+        },
+      }],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then(function(details) {
+      console.log(data,details);
+      alert('Transaction completed by ' + details.payer.name.given_name);
+    });
+  };
+
+  return (
+    <PayPalScriptProvider options={paypalOptions}>
+      <PayPalButtons
+        createOrder={createOrder}
+        onApprove={onApprove}
+      />
+    </PayPalScriptProvider>
+  );
+};
+
+export default PayPalButton;
